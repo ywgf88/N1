b3693d879ad2a48e9423af071524c3d582f41168
ywgf88 <43352863+ywgf88@users.noreply.github.com>
2021-05-30 08:10:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -71,8 +71,8 @@ git clone https://github.com/project-lede/luci-app-godproxy package/luci-app-god
 git clone https://github.com/garypang13/smartdns-le package/smartdns-le
 
 #svn co https://github.com/garypang13/openwrt-packages/tree/master/luci-app-smartdns
-svn co https://github.com/garypang13/openwrt-packages/tree/master/tcping
-svn co https://github.com/garypang13/openwrt-packages/tree/master/lua-maxminddb
+#svn co https://github.com/garypang13/openwrt-packages/tree/master/tcping
+#svn co https://github.com/garypang13/openwrt-packages/tree/master/lua-maxminddb
 
 #修改bypass的makefile
 #find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
